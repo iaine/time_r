fda42f697df678a314a3fc9a3d78805bb5533497
Iain Emsley <iain.emsley@warwick.ac.uk>
2025-07-09 21:17:37 +0100

    Update build_images.sh
    Beginning to refactor with functions

--- build_images.sh
+++ build_images.sh
@@ -1,10 +1,24 @@
 #/bin/bash
 
+FUNC=$1
+
+buildContainers () {
 #build the images using buildx for both architectures
 docker buildx build --platform linux/amd64 -t amdr .
 
 docker buildx build --platform linux/arm64 -t armr .
+}
 
+testRBuildTime() {
 docker run -t amdr:latest /bin/bash -c "mkdir workspaces && cd workspaces && git clone https://github.com/r-devel/r-dev-env.git && cd r-dev-env && wget https://raw.githubusercontent.com/iaine/time_r/refs/heads/main/time_build_r.sh && chmod +x time_build_r.sh && ./time_build_r.sh"
 
-docker run -t armr:latest /bin/bash -c "mkdir workspaces && cd workspaces && git clone https://github.com/r-devel/r-dev-env.git && cd r-dev-env && wget https://raw.githubusercontent.com/iaine/time_r/refs/heads/main/time_build_r.sh && chmod +x time_build_r.sh && ./time_build_r.sh"
+docker run -t armr:latest /bin/bash -c "mkdir workspaces && cd workspaces && git clone https://github.com/r-devel/r-dev-env.git && cd r-dev-env && wget https://raw.githubusercontent.com/iaine/time_r/refs/heads/main/time_build_r.sh && chmod +x time_build_r.sh && ./time_build_r.sh && cat /workspaces/r-dev-env/times.txt"
+}
+
+if ["$FUNC" == "build"]
+then
+    buildContainers
+elif
+    buildContainers
+    testRBuildTime
+fi
